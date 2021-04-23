8b0774e384510ac7fe585332f7475c1b8a66fbfb
Emilianolight <emiliano.mujica@correo.unimet.edu.ve>
2021-04-22 20:24:40 -0400

    commit

---
+++ src/main.java
@@ -0,0 +1,20 @@
+
+import java.util.Arrays;
+
+
+public class main {
+
+    public static void main(String[] args) {
+      
+        
+       Pelicula peliculas = new Pelicula();
+        Interfaz inter = new Interfaz();
+        inter.setVisible(true);
+         inter.setLocationRelativeTo(null);
+    
+      
+         
+
+    }
+
+}
